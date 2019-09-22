f18149dad8d49110b1ea4f2f3bd548c26867ceb7
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-22 20:46:32 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,13 @@
 class Backer 
   
+  attr_accessor :name
+  
   def initialize(name)
     @name = name 
   end 
   
-  def name 
-    @name 
-  end 
+  # def name 
+  #   @name 
+  # end 
   
 end 
