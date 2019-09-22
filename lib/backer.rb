751f10758e9c919bb3c499e20fb1f71e1dabce3c
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-22 20:53:02 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,13 @@
 class Backer 
   
-  attr_accessor :name, :backed_projects, :project
+  attr_accessor :name, :backed_projects
   
   def initialize(name)
     @name = name 
     @backed_projects = []
   end 
   
-  def back_projects(project)
+  def back_project(project)
     @back_projects << project
   end 
 
